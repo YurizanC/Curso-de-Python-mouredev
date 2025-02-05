aa35473670a800137d6ded4fb7cd069667d96470
Yurizan Cadavid <yurizacadavid@gmail.com>
2025-02-05 09:50:33 -0500

    Tipo de datos

--- hello.py
+++ hello.py
@@ -1,9 +1,22 @@
 # Esto es un comentatio: Hola Mundo
 # Nuestro hola mundo en python
-print('Hola Mundo')
+print("Hola Python")
+print('Hola Python')
+
+
 
 """
     Este es un
     comentario
     en varias lineas
 """
+
+'''
+    Este también es un
+    comentario
+    en varias li
+'''
+
+# Consultar el tipo de dato
+print (type("Hola Python")) # Tipo 'str'
+print (type(5)) # Tipo 'int'
